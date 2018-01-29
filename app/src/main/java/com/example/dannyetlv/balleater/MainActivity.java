85689181948387f533ae7f3c0f6ab9d29d88a413
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2018-01-29 15:06:25 +0200

    Preparing the application design v4 Solved bug (I set before twice the setY on a ball)

--- app/src/main/java/com/example/dannyetlv/balleater/MainActivity.java
+++ app/src/main/java/com/example/dannyetlv/balleater/MainActivity.java
@@ -15,52 +15,80 @@ package com.example.dannyetlv.balleater;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private TextView scoreLabel;
+
     private TextView startLabel;
+
     private ImageView box;
+
     private ImageView red;
+
     private ImageView yellow;
+
     private ImageView blue;
+
     private ImageView bomb;
 
 
-    //Position
     private int boxY;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scoreLabel = (TextView) findViewById(R.id.scoreLabel);
-        startLabel = (TextView) findViewById(R.id.startLabel);
+    scoreLabel = (TextView) findViewById(R.id.scoreLabel);
+    startLabel = (TextView) findViewById(R.id.startLabel);
 
-        box = (ImageView) findViewById(R.id.scoreLabel);
-        red = (ImageView) findViewById(R.id.red);
-        yellow = (ImageView) findViewById(R.id.yellow);
-        blue = (ImageView) findViewById(R.id.blue);
-        bomb = (ImageView) findViewById(R.id.bomb);
 
-        //Move to out of screen
-        red.setX(-80);
-        red.setY(-80);
-        yellow.setY(-80);
-        yellow.setY(-80);
-        blue.setY(-80);
-        blue.setY(-80);
-        bomb.setY(-80);
-        bomb.setY(-80);
+    box = (ImageView) findViewById(R.id.box);
 
-        //Temporary
-        startLabel.setVisibility(View.INVISIBLE);
-        boxY = 500;
+    red = (ImageView) findViewById(R.id.red);
 
+    yellow = (ImageView) findViewById(R.id.yellow);
+
+    blue = (ImageView) findViewById(R.id.blue);
+
+    bomb = (ImageView) findViewById(R.id.bomb);
+
+
+    //Move to out of screen
+
+    red.setX(-80);
+
+    red.setY(-80);
+
+    yellow.setX(-80);
+
+    yellow.setY(-80);
+
+    blue.setX(-80);
+
+    blue.setY(-80);
+
+    bomb.setX(-80);
+
+    bomb.setY(-80);
+
+    //Temporary
+
+    startLabel.setVisibility(View.INVISIBLE);
+
+    boxY = 500;
     }
+
+
+
+
 }
