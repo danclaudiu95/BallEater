1598aba31a085c1e636a537f6f3595d3678c5d55
danclaudiu95 <32520044+danclaudiu95@users.noreply.github.com>
2018-01-29 22:20:58 +0200

    Added alpha, beta, gamma for gyroscop sensors

--- app/src/main/java/com/example/dannyetlv/balleater/MainActivity.java
+++ app/src/main/java/com/example/dannyetlv/balleater/MainActivity.java
@@ -13,9 +13,15 @@
 
 package com.example.dannyetlv.balleater;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -28,9 +34,15 @@ import org.w3c.dom.Text;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+import static android.util.Half.EPSILON;
 
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private static final float NS2S = 1.0f / 1000000000.0f;
+    private final float[] deltaRotationVector = new float[4];
+    private float timestamp;
         private TextView scoreLabel;
 
         private TextView startLabel;
@@ -66,7 +78,10 @@ public class MainActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+            mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+            mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
+            mSensorManager.registerListener(this,mSensor,SensorManager.SENSOR_DELAY_NORMAL);
         scoreLabel = (TextView) findViewById(R.id.scoreLabel);
         startLabel = (TextView) findViewById(R.id.startLabel);
 
@@ -108,9 +123,58 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.v("MainActivity","Value of X: "+event.values[0]);
+        Log.v("MainActivity","Value of Y: "+event.values[1]);
+        Log.v("MainActivity","Value of Z: "+event.values[2]);
+        // This timestep's delta rotation to be multiplied by the current rotation
+        // after computing it from the gyro sample data.
+        if (timestamp != 0) {
+            final float dT = (event.timestamp - timestamp) * NS2S;
+            // Axis of the rotation sample, not normalized yet.
+            float axisX = event.values[0];
+            float axisY = event.values[1];
+            float axisZ = event.values[2];
+
+            // Calculate the angular speed of the sample
+            float omegaMagnitude =(float) Math.sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
+
+            // Normalize the rotation vector if it's big enough to get the axis
+            // (that is, EPSILON should represent your maximum allowable margin of error)
+            if (omegaMagnitude > EPSILON) {
+                axisX /= omegaMagnitude;
+                axisY /= omegaMagnitude;
+                axisZ /= omegaMagnitude;
+            }
+
+            // Integrate around this axis with the angular speed by the timestep
+            // in order to get a delta rotation from this sample over the timestep
+            // We will convert this axis-angle representation of the delta rotation
+            // into a quaternion before turning it into the rotation matrix.
+            float thetaOverTwo = omegaMagnitude * dT / 2.0f;
+            float sinThetaOverTwo = (float) Math.sin(thetaOverTwo);
+            float cosThetaOverTwo = (float) Math.cos(thetaOverTwo);
+            deltaRotationVector[0] = sinThetaOverTwo * axisX;
+            deltaRotationVector[1] = sinThetaOverTwo * axisY;
+            deltaRotationVector[2] = sinThetaOverTwo * axisZ;
+            deltaRotationVector[3] = cosThetaOverTwo;
+        }
+        timestamp = event.timestamp;
+        float[] deltaRotationMatrix = new float[9];
+        SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
+      TextView senz=(TextView)findViewById(R.id.sensorID);
+      senz.setText("X="+Float.toString(deltaRotationVector[0])+"  Y="+Float.toString(deltaRotationVector[1])+"  Z="+Float.toString(deltaRotationVector[2]));
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
 
 
-        public void changePos() {
+    public void changePos() {
 
 
             //Move Box
